5bf666834835786bcbf04bb839eb23712a4808db
douglastieni <douglasshikutwa@gmail.com>
2026-09-16 13:31:34 +0100

    Added logic math logic

---
+++ script.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}   
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}
+function calculate(operation, a, b) {
+    switch (operation) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            throw new Error("Invalid operation");
+    }
+}
